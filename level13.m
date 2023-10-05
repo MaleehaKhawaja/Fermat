151049db04ddbc96fe91305161e67c58711c2c29
MaleehaKhawaja <65968445+MaleehaKhawaja@users.noreply.github.com>
2023-10-05 09:08:14 +0100

    Create level13.m

---
+++ level13.m
@@ -0,0 +1,27 @@
+K<th>:=CompositeFields(QuadraticField(2),QuadraticField(3))[1];
+
+OK:=MaximalOrder(K);
+
+P:=Factorisation(2*OK)[1,1];
+
+assert Valuation(2*OK,P) eq 4; // totally ramified
+
+
+// Want the map Phi : OK^* --> R^* / (R^*)^2     R=OK/P^9
+
+
+R,pi:=quo<OK | P^6>;
+
+V,mu:=UnitGroup(R);  // mu : V --> R     V=R*
+
+W,k:=quo<V | 2*V>;  // W = R^* / (R^*)^2
+
+U,phi:=UnitGroup(OK);   // U = OK*
+
+G:=OrderedGenerators(U);
+
+Phi := hom< U ->  W |  [ k((pi(phi(g)))@@mu) : g in G ] >;
+
+Q,l := quo< W | Image(Phi) >;
+
+asswert #Q eq 1;
